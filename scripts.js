002b5500705e446509afc4ec638c2a3a2840a5f3
flavioh01 <149084095+flavioh01@users.noreply.github.com>
2024-09-02 23:10:48 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -8,14 +8,4 @@ function toggleBackground() {
     document.body.classList.toggle('white-bg');
 }
 
-function showBook(bookId) {
-    const bookDetails = {
-        livro1: 'Sinopse do Livro 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id sapien vitae nunc faucibus dignissim.',
-        livro2: 'Sinopse do Livro 2: Integer in diam ut ligula sodales elementum. Proin ac lectus ut nisi bibendum pharetra.',
-        livro3: 'Sinopse do Livro 3: Suspendisse potenti. Phasellus viverra est sit amet libero dictum, a fermentum ligula convallis.',
-        livro4: 'Sinopse do Livro 4: Nam et lorem a sapien lacinia elementum. Morbi ut orci at nulla lacinia tempor.',
-        livro5: 'Sinopse do Livro 5: Duis sed lectus et odio scelerisque euismod. Curabitur euismod urna sed ligula euismod, vel sagittis purus gravida.'
-    };
 
-    document.getElementById('book-details').innerText = bookDetails[bookId];
-}
